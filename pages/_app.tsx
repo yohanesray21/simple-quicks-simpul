6a513588f24d23fa97f33bb33072f9e85adfdb37
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-04-12 11:22:31 +0800

    first commit adding mantine ui as css framework

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: 'light' }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
